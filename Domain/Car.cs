891fd73ad0f71ba14b8707dc9b074055e3e4d126
Kalle Blomnell <Karlmikaelb@gmail.com>
2018-12-10 17:29:11 +0100

    Wrapping and messagebody

--- Domain/Car.cs
+++ Domain/Car.cs
@@ -1,22 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Car
+    [MessageContract(IsWrapped = true,
+        WrapperName = "CarRequestObject",
+        WrapperNamespace = "http://localhost:8080/Cars")]
+
+    public class CarRequest
     {
-        public int Id { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public string RegNr { get; set; }
+        [MessageHeader(Namespace = "http://localhost:8080/Cars")]
+        public string LicenseKey { get; set; }
 
-        public void Add(Car car)
-        {
-
-        }
+        [MessageBodyMember(Namespace = "http://localhost:8080/Cars")]
+        public int CarId { get; set; }
     }
+
+    [MessageContract(IsWrapped = true,
+       WrapperName = "CarInfoObject",
+       WrapperNamespace = "http://localhost:8080/Cars")]
+
+    public class CarInfo
+    {
+        public CarInfo() { }
+
+        public CarInfo(Car car)
+        {
+            this.Id = car.Id;
+            this.CarId = car.CarId;
+            this.Brand = car.Brand;
+            this.Model = car.Model;
+            this.Year = car.Year;
+            this.RegNr = car.RegNr;
+        }
+
+        [MessageBodyMember(Order = 1, Namespace = "http://localhost:8080/Cars")]
+        public int Id { get; set; }
+
+        [MessageBodyMember(Order = 2, Namespace = "http://localhost:8080/Cars")]
+        public int CarId { get; set; }
+
+        [MessageBodyMember(Order = 3, Namespace = "http://localhost:8080/Cars")]
+        public string Brand { get; set; }
+
+        [MessageBodyMember(Order = 4, Namespace = "http://localhost:8080/Cars")]
+        public string Model { get; set; }
+
+        [MessageBodyMember(Order = 5, Namespace = "http://localhost:8080/Cars")]
+        public int Year { get; set; }
+
+        [MessageBodyMember(Order = 6, Namespace = "http://localhost:8080/Cars")]
+        public string RegNr { get; set; }
+    }
+}
+
+[DataContract(Namespace = "http://localhost:8080/Cars")]
+public class Car
+{
+    [DataMember(Order = 1, Name = "ID")]
+    public int Id { get; set; }
+
+    [DataMember(Order = 2)]
+    public int CarId { get; set; }
+
+    [DataMember(Order = 3)]
+    public string Brand { get; set; }
+
+    [DataMember(Order = 4)]
+    public string Model { get; set; }
+
+    [DataMember(Order = 5)]
+    public int Year { get; set; }
+
+    [DataMember(Order = 6)]
+    public string RegNr { get; set; }
 }
--- Domain/Customer.cs
+++ Domain/Customer.cs
@@ -1,17 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
+    [MessageContract(IsWrapped = true,
+        WrapperName = "CustomerRequestObject",
+        WrapperNamespace = "http://localhost:8080/Customer")]
+
+    public class CustomerRequest
+    {
+        [MessageHeader(Namespace = "http://localhost:8080/Customer")]
+        public string LicenseKey { get; set; }
+
+        [MessageBodyMember(Namespace = "http://localhost:8080/Customer")]
+        public int CustomerId { get; set; }
+    }
+
+    [MessageContract(IsWrapped = true,
+       WrapperName = "CustomerInfoObject",
+       WrapperNamespace = "http://localhost:8080/Customer")]
+
+    public class CustomerInfo
+    {
+        public CustomerInfo() { }
+
+        public CustomerInfo(Customer customer)
+        {
+            this.Id = customer.Id;
+            this.Firstname = customer.Firstname;
+            this.Lastname = customer.Lastname;
+            this.Telephone = customer.Telephone;
+            this.Email = customer.Email;
+        }
+
+        [MessageBodyMember(Order = 1, Namespace = "http://localhost:8080/Customer")]
+        public int Id { get; set; }
+
+        [MessageBodyMember(Order = 2, Namespace = "http://localhost:8080/Customer")]
+        public string Firstname { get; set; }
+
+        [MessageBodyMember(Order = 3, Namespace = "http://localhost:8080/Customer")]
+        public string Lastname { get; set; }
+
+        [MessageBodyMember(Order = 4, Namespace = "http://localhost:8080/Customer")]
+        public string Telephone { get; set; }
+
+        [MessageBodyMember(Order = 5, Namespace = "http://localhost:8080/Customer")]
+        public string Email { get; set; }
+    }
+    [DataContract(Namespace = "http://localhost:8080/Customer")]
     public class Customer
     {
+        [DataMember(Order = 1, Name = "ID")]
         public int Id { get; set; }
+
+        [DataMember(Order = 2)]
         public string Firstname { get; set; }
+
+        [DataMember(Order = 3)]
         public string Lastname { get; set; }
+
+        [DataMember(Order = 4)]
         public string Telephone { get; set; }
+
+        [DataMember(Order = 5)]
         public string Email { get; set; }
     }
 }
